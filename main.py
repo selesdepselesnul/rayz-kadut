e46df5d22571b185cbfac820dc71e180ea0fb116
selesdepselesnul <selesdepselesnul@gmail.com>
2016-03-17 19:04:18 +0700

    add main loop

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
 
 def main():
-	pass
+	pygame.init()
 
+	running = True
+	resolution = (600, 400)
+	pygame.display.set_caption('Rayz Kadut')
+	main_surface = pygame.display.set_mode(resolution)
+
+	while running:
+		for event in pygame.event.get():
+			if event.dict.get('key') == pygame.K_q:
+				running = False
+				pygame.quit()
+				quit()
+	
 if __name__ == '__main__':
 	main()
