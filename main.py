b497d5d04444b7984e337ac889e3165cb0f7dfa2
selesdepselesnul <selesdepselesnul@gmail.com>
2016-03-17 20:35:41 +0700

    refactor

--- main.py
+++ main.py
@@ -14,15 +14,19 @@ class SnakeSprite(pygame.sprite.Sprite):
 	WIDTH = 20
 	HEIGHT = 60
 
-	def __init__(self):
+	def __init__(self, surface):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load('snake.png')
 		self.rect = self.image.get_rect()
+		self.surface = surface
 		
-	def update(self, source):
-		source.blit(self.image, 
-			(MainController.RESOLUTION[0]/2 - SnakeSprite.WIDTH,
-			 MainController.RESOLUTION[1] - SnakeSprite.HEIGHT))
+	def update(self, coordinat = None):
+		if coordinat:
+			self.surface.blit(self.image, (coordinat[0], coordinat[1]))
+		else:	
+			self.surface.blit(self.image, 
+				(MainController.RESOLUTION[0]/2 - SnakeSprite.WIDTH,
+			 	MainController.RESOLUTION[1] - SnakeSprite.HEIGHT))
 
 def main():
 	pygame.init()
@@ -34,8 +38,8 @@ def main():
 	
 	main_surface = pygame.display.set_mode(resolution)
 	main_surface.fill(Color.WHITE)
-	snake_sprite = SnakeSprite()
-	snake_sprite.update(main_surface)
+	snake_sprite = SnakeSprite(main_surface)
+	snake_sprite.update()
 	
 	while running:
 		for event in pygame.event.get():
@@ -43,6 +47,9 @@ def main():
 				running = False
 				pygame.quit()
 				quit()
+
+			# if event.dict.get('key') == pygame.K_RIGHT:
+			# 	snake_sprite.update()
 		pygame.display.update()			
 		
 if __name__ == '__main__':
